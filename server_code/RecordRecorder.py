ec2ab39c462b4ea860d1e45ab4b78f938d85707b
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-06 13:09:20 +0000

    Edited RecordRecorder

--- server_code/RecordRecorder.py
+++ server_code/RecordRecorder.py
@@ -9,7 +9,21 @@ import json
 def record_machines(url, api_key):
   data=anvil.server.call('get_machines', url, api_key)
   for machine in data['machines']:
-    app_tables.machines.add_row(name=machine['name'], id=machine['id'], ipAddr=machine['ipAddresses'])
+    app_tables.machines.add_row(name=machine['name'], id=machine['id'], ipAddr=machine['ipAddresses'], 
+                                online=machine['online'], machine['user']
+                                machine['lastSeen']
+                                machine['expiry']
+                                machine['registerMethod']
+                                machine['discoKey']
+                                machine['createdAt']
+                                machine['invalidTags']
+machine['validTags']
+machine['machineKey']
+machine['lastSuccessfulUpdate']
+machine['givenName']
+machine['forcedTags']
+machine['nodeKey']
+machine['preAuthKey'])
     print(machine['name'])
     print(machine['id'])
     print(machine['ipAddresses'])
