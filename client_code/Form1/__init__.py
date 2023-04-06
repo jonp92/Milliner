4ffc96c7c2b878b41dd31049b584d569553c9883
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-06 00:38:49 +0000

    Created app

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,9 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
