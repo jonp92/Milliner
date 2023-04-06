cab6730932af186d781137f42a5c958cd50284b2
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-06 23:57:56 +0000

    Edited Machines and Home

--- client_code/Machines/__init__.py
+++ client_code/Machines/__init__.py
@@ -8,6 +8,8 @@ from anvil.tables import app_tables
 
 class Machines(MachinesTemplate):
   def __init__(self, **properties):
+    self.url = [r['url'] for r in app_tables.settings.search()][0]
+    self.api_key = [r['api_key'] for r in app_tables.settings.search()][0]
     self.item = anvil.server.call('get_machine_table')
     self.init_components(**properties)
     self.repeating_panel_machines.items = self.item.search()
@@ -18,5 +20,18 @@ class Machines(MachinesTemplate):
   def button_add_machine_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.column_panel_add_machine.visible = True
+
+  def button_save_new_machine_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert(anvil.server.call('register_machine', self.url, self.api_key, self.text_box_nodekey.text, self.drop_down_1.selected_value))
+    self.refresh_data_bindings()
+
+  def button_cancel_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.column_panel_add_machine.visible = False
+
+
+
+    
 
 
