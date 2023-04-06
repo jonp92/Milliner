df8b249f75406d2d14744f03748144660fc4a63c
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-06 01:20:48 +0000

    Edited settings and API_Interface

--- server_code/API_Interface.py
+++ server_code/API_Interface.py
@@ -5,15 +5,14 @@ from anvil.tables import app_tables
 import anvil.server
 import requests
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+
+
+def test_api_key(url, api_key):
+    response = requests.get(
+        str(url)+"/api/v1/apikey",
+        headers={
+            'Accept': 'application/json',
+            'Authorization': 'Bearer '+str(api_key)
+        }
+    )
+    return response.status_code
