c603482fcabcb77eb3bd0c0f9a97f89e05e2f906
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-07 15:46:13 +0000

    Edited Machines.MachineDetail and Machines.MachineDetail.RowTemplate2

--- client_code/Machines/MachineDetail/RowTemplate2/__init__.py
+++ client_code/Machines/MachineDetail/RowTemplate2/__init__.py
@@ -8,15 +8,7 @@ from anvil.tables import app_tables
 
 class RowTemplate2(RowTemplate2Template):
   def __init__(self, **properties):
-    self.prefixes = [route['prefix'] for route in self.item['routes']]
-    self.advertised = [route['advertised'] for route in self.item['routes']]
-    self.online = [route['machine']['online'] for route in self.item['routes']]
-    self.enabled = [route['enabled'] for route in self.item['routes']]
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.label_prefixes.text = self.prefixes
-    self.label_advertised.text = self.advertised
-    self.label_online.text = self.online
-    self.label_enabled.text = self.enabled
-
+    self.label_online.text = self.item['machine']['online']
     # Any code you write here will run before the form opens.
