da6c554bde395708038969e46c9eb287acf93f33
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-07 00:58:37 +0000

    Created Users.RowTemplate5; Edited Users, Machines.RowTemplate2 and Home

--- client_code/Users/RowTemplate5/__init__.py
+++ client_code/Users/RowTemplate5/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import UsersTemplate
+from ._anvil_designer import RowTemplate5Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,10 +6,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Users(UsersTemplate):
+class RowTemplate5(RowTemplate5Template):
   def __init__(self, **properties):
-    self.item = anvil.server.call('get_hs_users_table').search()
+    # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    #self.repeating_panel_users.items = self.item.search()
 
     # Any code you write here will run before the form opens.
