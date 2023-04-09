e73abe5fcbbcc6d2980a2570e7817566ecf34108
jonathan@pressler.tech <jonathan@pressler.tech>
2023-04-09 01:06:05 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -15,14 +15,14 @@ class Home(HomeTemplate):
     self.version = 'v.0.1.0'
     fresh_install = anvil.server.call('check_users_table')
     if not anvil.users.get_user():
-      self.url = None
-      self.api_key = None
-      #self.label_sync_time.text = 'Never Synced'
+      self.url = ''
+      self.api_key = ''
+      self.label_sync_time.text = 'Never Synced'
     else:
       self.url = [r['url'] for r in app_tables.settings.search()][0]
       self.api_key = [r['api_key'] for r in app_tables.settings.search()][0]
-      #self.label_sync_time.text = [r['last_hs_sync'] for r in app_tables.settings.search()][0]
-    self.item = anvil.server.call('get_machine_table').search(online=True)
+      self.label_sync_time.text = [r['last_hs_sync'] for r in app_tables.settings.search()][0]
+      self.item = anvil.server.call('get_machine_table').search(online=True)
     self.user = anvil.users.get_user()
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
